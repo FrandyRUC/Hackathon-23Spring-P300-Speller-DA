b2c93a70397409f334a2c101d217d5a2a9eaf3ee
yangxu <xu.yang1@corp.elong.com>
2023-04-21 17:43:05 -0700

    CNN DNN XGB

--- src/all_models.py
+++ src/all_models.py
@@ -50,8 +50,10 @@ from tensorflow.python.keras.optimizer_v2.adam import Adam
 from keras import regularizers
 
 from tensorflow.python.keras.utils.np_utils import to_categorical
+from keras.layers import Dropout
+from keras.regularizers import l2
 
-def data_process_3():
+def data_process(nn):
     mats = []
 
     for i in range(1, 6):
@@ -106,8 +108,9 @@ def data_process_3():
     X_np = np.array(X)
     y_np = np.array(y)
     X_train, X_test, y_train, y_test = train_test_split(X_np, y_np, test_size=0.2, random_state=4)
-    y_train = to_categorical(y_train, num_classes=2)
-    y_test = to_categorical(y_test, num_classes=2)
+    if nn:
+        y_train = to_categorical(y_train, num_classes=2)
+        y_test = to_categorical(y_test, num_classes=2)
     return X_train, y_train, X_test, y_test
 
 
@@ -118,8 +121,10 @@ def model_cnn(X_train, y_train,X_test, y_test):
     from keras.callbacks import CSVLogger, ModelCheckpoint
 
     # 设置输入维度
-    input_shape = (275, 8)
-
+    input_shape = (X_train.shape[1],X_train.shape[2])
+    print("input_shape:",input_shape)
+    # input_shape = (275, 8)
+    # 275= (0.1+1)*250
     # 定义模型
     model = tf.keras.Sequential([
         # 卷积层1
@@ -154,16 +159,106 @@ def model_cnn(X_train, y_train,X_test, y_test):
     # check the model performance on test data
     scores = model.evaluate(X_test, y_test)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-
+    y_pred = model.predict(X_test)
+    auc_score = roc_auc_score(y_test, y_pred)
+    print("CNN -AUC Score: ", auc_score)
     # # 打印模型结构
     # model.summary()
     return model
 
+def model_dnn(X_train, y_train, X_test, y_test):
+    X_train = X_train.reshape(X_train.shape[0], -1)
+    X_test = X_test.reshape(X_test.shape[0], -1)
+    model = Sequential()
+    model.add(Dense(512, input_shape=(X_train.shape[1],), activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+    model.add(Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(Dense(64, activation='relu',kernel_regularizer=regularizers.l2(0.01)))
+    model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(Dense(2, activation='softmax'))
+    # model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_test, y_test))
+
+    loss, accuracy = model.evaluate(X_test, y_test)
+    y_pred = model.predict(X_test)
+    auc_score = roc_auc_score(y_test, y_pred)
+    print("DNN - AUC Score: ", auc_score)
+    print('DNN Test Loss:', loss)
+    print('DNN Test Accuracy:', accuracy)
+
+
+def model_lstm(X_train, y_train, X_test, y_test):
+    model = Sequential()
+
+    # 添加LSTM层和Dense层
+    model.add(LSTM(units=128, input_shape=(X_train.shape[1], X_train.shape[2]), kernel_regularizer=l2(0.05)))
+    model.add(Dropout(0.2))  # 添加 Dropout 层
+    model.add(Dense(units=2, activation='softmax', kernel_regularizer=l2(0.05)))
+
+    # 编译模型
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # 训练模型
+    model.fit(X_train, y_train, epochs=10, batch_size=32,  validation_data=(X_test, y_test))
+
+    # 评估模型
+    loss, accuracy = model.evaluate(X_test, y_test)
+    y_pred = model.predict(X_test)
+    auc_score = roc_auc_score(y_test, y_pred)
+    print("LSTM -AUC Score: ", auc_score)
+    print('LSTM -Test Loss:', loss)
+    print('LSTM -Test Accuracy:', accuracy)
+
+def model_xgb(X_train, y_train, X_test, y_test):
+
+
+    import xgboost as xgb
+    X_train = X_train.reshape(X_train.shape[0], -1)
+    X_test = X_test.reshape(X_test.shape[0], -1)
+
+    model = xgb.XGBClassifier(
+        objective='binary:logistic',
+        n_estimators=800,
+        max_depth=3,
+        learning_rate=0.1,
+        subsample=0.7,
+        colsample_bytree=0.5,
+        reg_alpha=5, # L1正则化项系数
+        reg_lambda=100, # L2正则化项系数
+        random_state=42
+    )
+    eval_set = [(X_train, y_train), (X_test, y_test)]
+    model.fit(X_train, y_train, eval_set=eval_set, eval_metric=['error', 'auc'], verbose=True)
+    # Testing
+    y_pred = model.predict(X_test)
+    # auc_score = roc_auc_score(y_test, y_pred)
+    # print("XGB -AUC Score: ", auc_score)
+    # accuracy = accuracy_score(y_test, y_pred)
+    # print("XGB Accuracy:", accuracy)
+
+    # 计算准确率和AUC
+    accuracy = 1 - model.evals_result()['validation_1']['error'][model.best_iteration]
+    auc = model.evals_result()['validation_1']['auc'][model.best_iteration]
+
+    print("XGB Accuracy:", accuracy)
+    print("XGB AUC:", auc)
+
 
 if __name__ == "__main__":
 
-    X_train, y_train, X_test, y_test = data_process_3()
-    print(X_train.shape)
-    print(X_test.shape)
-    model_cnn(X_train, y_train, X_test, y_test)
-    pass
+    # X_train, y_train, X_test, y_test = data_process(nn=True)
+    # print(X_train.shape)
+    # print(X_test.shape)
+    # print(y_train.shape)
+    # print(y_test.shape)
+    # model_cnn(X_train, y_train, X_test, y_test)
+    # model_dnn(X_train, y_train, X_test, y_test)
+    # model_lstm(X_train, y_train, X_test, y_test)
+    X_train, y_train, X_test, y_test = data_process(nn=False)
+    model_xgb(X_train, y_train, X_test, y_test)
+
+
+
