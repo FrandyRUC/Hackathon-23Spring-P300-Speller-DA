86e3901de8b8322627406c9997534052489fb2aa
韩瑞敏 <121203548@qq.com>
2023-04-23 01:31:05 -0700

    seperate the test and training dataset

--- src/all_models_rm.py
+++ src/all_models_rm.py
@@ -53,10 +53,127 @@ from tensorflow.python.keras.utils.np_utils import to_categorical
 from keras.layers import Dropout
 from keras.regularizers import l2
 
+def data_process_test(nn):
+    mats = []
+    lst=[4,]
+    for i in lst:
+        mats.append(scipy.io.loadmat(f'../p300_datasets/S{i}.mat'))
+
+    SAMPLING_FREQUENCY = 250
+    PRE_SAMPLES = int(0.1 * SAMPLING_FREQUENCY)  # -0.1
+    POST_SAMPLES = int(1.0 * SAMPLING_FREQUENCY)  # 0.7
+
+    p_samples = {}
+    n_samples = {}
+    # print('fs',mats[0]['fs'])
+    # print('trig', len(mats[0]['trig']))
+    # print('y', mats[0]['y'])
+    files_lst = range(len(mats))
+    for s in files_lst:
+        p_samples[s] = []
+        n_samples[s] = []
+        for i in range(len(mats[s]['trig'])):
+            if mats[s]['trig'][i] == 0:
+                pass  # ignore this
+            elif mats[s]['trig'][i] == 1:
+                p_samples[s].append((i - PRE_SAMPLES, i + POST_SAMPLES))
+            elif mats[s]['trig'][i] == -1:
+                n_samples[s].append((i - PRE_SAMPLES, i + POST_SAMPLES))
+    y = []
+    X = []
+    for subject_index  in files_lst:
+        for i in range(len(p_samples[subject_index])):
+            sample = mats[subject_index]['y'][p_samples[subject_index][i][0]:p_samples[subject_index][i][1], :]
+            X.append(sample)
+            y.append(1)
+
+        for i in range(len(n_samples[subject_index])):
+            sample = mats[subject_index]['y'][n_samples[subject_index][i][0]:n_samples[subject_index][i][1], :]
+            X.append(sample)
+            y.append(0)
+
+    print(len(X))
+    print(len(X[0][0]))
+    X_np = np.array(X)
+    y_np = np.array(y)
+    # X_train, X_test, y_train, y_test = train_test_split(X_np, y_np, test_size=0.2, random_state=4)
+    if nn:
+        y_np= to_categorical(y_np, num_classes=2)
+        # y_train = to_categorical(y_train, num_classes=2)
+        # y_test = to_categorical(y_test, num_classes=2)
+    return X_np, y_np
+    # return X_train, y_train, X_test, y_test
+
+def data_process_train(nn):
+    mats = []
+    lst=[1,2,3,5]
+    for i in lst:
+        mats.append(scipy.io.loadmat(f'../p300_datasets/S{i}.mat'))
+
+    SAMPLING_FREQUENCY = 250
+    PRE_SAMPLES = int(0.1 * SAMPLING_FREQUENCY)  # -0.1
+    POST_SAMPLES = int(1.0 * SAMPLING_FREQUENCY)  # 0.7
+
+    p_samples = {}
+    n_samples = {}
+    # print('fs',mats[0]['fs'])
+    # print('trig', len(mats[0]['trig']))
+    # print('y', mats[0]['y'])
+    files_lst = range(len(mats))
+    for s in files_lst:
+        p_samples[s] = []
+        n_samples[s] = []
+        for i in range(len(mats[s]['trig'])):
+            if mats[s]['trig'][i] == 0:
+                pass  # ignore this
+            elif mats[s]['trig'][i] == 1:
+                p_samples[s].append((i - PRE_SAMPLES, i + POST_SAMPLES))
+                p_samples[s].append((i - PRE_SAMPLES + 1, i + POST_SAMPLES + 1))
+                p_samples[s].append((i - PRE_SAMPLES - 1, i + POST_SAMPLES - 1))
+            elif mats[s]['trig'][i] == -1:
+                n_samples[s].append((i - PRE_SAMPLES, i + POST_SAMPLES))
+                n_samples[s].append((i - PRE_SAMPLES + 1, i + POST_SAMPLES + 1))
+                n_samples[s].append((i - PRE_SAMPLES - 1, i + POST_SAMPLES - 1))
+
+    # let's consider one subject for now, and then we can concat all together after if we want
+    # consider subject 0 for now
+
+    # for i in range(1,6):
+    #     print(len(p_samples[subject_index]),len(n_samples[subject_index]))
+
+    # for subject_index in files_lst:
+    subject_index = 2
+    # x = mats[subject_index]['y']
+    num_cols = len(p_samples[subject_index]) + len(n_samples[subject_index])
+    y = []
+    X = []
+    for subject_index  in files_lst:
+        for i in range(len(p_samples[subject_index])):
+            sample = mats[subject_index]['y'][p_samples[subject_index][i][0]:p_samples[subject_index][i][1], :]
+            X.append(sample)
+            y.append(1)
+
+        for i in range(len(n_samples[subject_index])):
+            sample = mats[subject_index]['y'][n_samples[subject_index][i][0]:n_samples[subject_index][i][1], :]
+            X.append(sample)
+            y.append(0)
+
+    print(len(X))
+    print(len(X[0][0]))
+    X_np = np.array(X)
+    y_np = np.array(y)
+    # X_train, X_test, y_train, y_test = train_test_split(X_np, y_np, test_size=0.2, random_state=4)
+    if nn:
+        y_np= to_categorical(y_np, num_classes=2)
+        # y_train = to_categorical(y_train, num_classes=2)
+        # y_test = to_categorical(y_test, num_classes=2)
+    return X_np, y_np
+    # return X_train, y_train, X_test, y_test
+
 def data_process(nn):
     mats = []
 
-    for i in range(1, 6):
+    for i in range(1, 5):
         mats.append(scipy.io.loadmat(f'../p300_datasets/S{i}.mat'))
 
     SAMPLING_FREQUENCY = 250
@@ -115,7 +232,7 @@ def data_process(nn):
     print(len(X[0][0]))
     X_np = np.array(X)
     y_np = np.array(y)
-    X_train, X_test, y_train, y_test = train_test_split(X_np, y_np, test_size=0.2, random_state=4)
+    # X_train, X_test, y_train, y_test = train_test_split(X_np, y_np, test_size=0.2, random_state=4)
     if nn:
         y_train = to_categorical(y_train, num_classes=2)
         y_test = to_categorical(y_test, num_classes=2)
@@ -180,7 +297,7 @@ def model_cnn(X_train, y_train,X_test, y_test):
     early_stopping_acc = EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=20,
                                        restore_best_weights=True)
 
-    his = model.fit(X_train, y_train, epochs=160, batch_size=32,
+    his = model.fit(X_train, y_train, epochs=50, batch_size=32,
                     validation_data=(X_test, y_test), callbacks=[logger, early_stopping_acc])
 
     # Calculate the accuracy score on the test set using the best weights
@@ -272,44 +389,37 @@ def model_lstm(X_train, y_train, X_test, y_test):
 def model_xgb(X_train, y_train, X_test, y_test):
     from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
+    import numpy as np
     import xgboost as xgb
+    # 计算每个通道的均值和方差
+    mean_train = np.mean(X_train, axis=1)  # 计算所有样本和所有时间步上每个通道的均值，并转为一维数组
+    var_train = np.var(X_train, axis=1) # 计算所有样本和所有时间步上每个通道的方差，并转为一维数组
+    print("mean_train.shape", mean_train.shape)
+
+    mean_test = np.mean(X_test, axis=1)  # 计算所有样本和所有时间步上每个通道的均值，并转为一维数组
+    var_test = np.var(X_test, axis=1) # 计算所有样本和所有时间步上每个通道的方差，并转为一维数组
+
     X_train = X_train.reshape(X_train.shape[0], -1)
     X_test = X_test.reshape(X_test.shape[0], -1)
 
+    # 将每个通道的均值和方差连接在二维数组的后面
+    X_train = np.concatenate((X_train, mean_train, var_train), axis=1)
+    X_test = np.concatenate((X_test, mean_test, var_test), axis=1)
+    print("X_train.shape", X_train.shape)
+
     model = xgb.XGBClassifier(
         objective='binary:logistic',
-        use_label_encoder=False,
         n_estimators=800,
-        max_depth=3,
+        max_depth=2,
         learning_rate=0.1,
-        subsample=0.7,
-        colsample_bytree=0.5,
-        reg_alpha=5, # L1正则化项系数
-        reg_lambda=100, # L2正则化项系数
+        subsample=0.4,
+        colsample_bytree=0.3,
+        # reg_alpha=0.1, # L1正则化项系数
+        reg_lambda=1, # L2正则化项系数
         random_state=42
     )
-
-    # Hyperparameter tuning
-    param_grid = {
-        'n_estimators': [400, 600, 800, 1000],
-        'max_depth': [3, 5, 7],
-        'subsample': [0.6, 0.7, 0.8],
-        'colsample_bytree': [0.4, 0.5, 0.6],
-        'reg_alpha': [1, 5, 10],
-        'reg_lambda': [1, 10, 50]
-    }
-
-    grid_search = GridSearchCV(model, param_grid, cv=5, n_jobs=-1)
-    grid_search.fit(X_train, y_train)
-
-    model = grid_search.best_estimator_
-    print(f"Best hyperparameters: {grid_search.best_params_}")
-
     eval_set = [(X_train, y_train), (X_test, y_test)]
-    # print("The eval set is:",eval_set[])
-    model.fit(X_train, y_train, eval_set=eval_set, eval_metric=['error', 'auc'], verbose=False)
-
-
+    model.fit(X_train, y_train, eval_set=eval_set, eval_metric=['error', 'auc'], verbose=True)
     # Testing
     y_pred = model.predict(X_test)
     # auc_score = roc_auc_score(y_test, y_pred)
@@ -320,10 +430,11 @@ def model_xgb(X_train, y_train, X_test, y_test):
     # 计算准确率和AUC
     accuracy = 1 - model.evals_result()['validation_1']['error'][model.best_iteration]
     auc = model.evals_result()['validation_1']['auc'][model.best_iteration]
+    f1score = f1_score(y_test, y_pred)
 
     print("XGB Accuracy:", accuracy)
     print("XGB AUC:", auc)
-
+    print("XGB F1_SCORE:", f1score)
 
 def SVM_model(X_train, y_train, X_test, y_test):
     X_train = X_train.reshape(X_train.shape[0], -1)
@@ -343,17 +454,23 @@ def SVM_model(X_train, y_train, X_test, y_test):
 
 if __name__ == "__main__":
     import time
-    X_train, y_train, X_test, y_test = data_process(nn=True)
+    # X_train, y_train, X_test, y_test = data_process(nn=True)
 
+    X_train, y_train = data_process_train(nn=True)
+    X_test, y_test = data_process_test(nn=True)
     start_time = time.time()
+    # model_cnn(X_train, y_train, X_test, y_test)
     model_cnn(X_train, y_train, X_test, y_test)
 
     end_time = time.time()
     training_time = end_time - start_time
     print("Training time: {:.2f} seconds".format(training_time))
+
     # model_dnn(X_train, y_train, X_test, y_test)
     # model_lstm(X_train, y_train, X_test, y_test)
     # X_train, y_train, X_test, y_test = data_process(nn=False)
+    # X_train, y_train = data_process_train(nn=False)
+    # X_test, y_test = data_process_test(nn=False)
 
     # model_xgb(X_train, y_train, X_test, y_test)
 
